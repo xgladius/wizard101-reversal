ec29be51c083fc89faf0429e3bcd856df7f13d86
xGladius <xgladius@protonmail.com>
2021-09-15 13:41:03 -0600

    Update function_rename.py

--- idapy_scripts/function_rename.py
+++ idapy_scripts/function_rename.py
@@ -13,3 +13,20 @@ for func_adr in XrefsTo(error_sprintf_func, 0):
     name_str = ida_bytes.get_strlit_contents(real_str_adr, -1, ida_nalt.STRTYPE_C)
     idc.set_name(idc.get_func_attr(str_adr, idc.FUNCATTR_START), name_str, idc.SN_NOCHECK)
     
+## for later? versions of python
+import idaapi
+import idautils
+import idc
+
+error_sprintf_func = ida_search.find_binary(0, 0xfffffffffffffff, "4C 8B DC 48 83 EC 58", 16, idaapi.SEARCH_DOWN)
+
+for func_adr in XrefsTo(error_sprintf_func, 0):
+    f = func_adr.frm
+    str_adr = idc.find_binary(f, SEARCH_UP, "48 8D 05")
+    if str_adr == 0xffffffffffffffff:
+        continue
+    real_str_adr = str_adr + ida_bytes.get_wide_dword(str_adr + 3) + 7
+    name_str = ida_bytes.get_strlit_contents(real_str_adr, -1, ida_nalt.STRTYPE_C)
+    if name_str is None:
+        continue
+    idc.set_name(idc.get_func_attr(str_adr, idc.FUNCATTR_START), name_str.decode("utf-8"), idc.SN_NOCHECK)
